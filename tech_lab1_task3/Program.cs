48b16c2080f2e8134bb06e8e73f00e4fd7ffe23e
Pwuru <mihail_popov2005@mail.ru>
2025-03-03 12:07:41 +0800

    Добавьте файлы проекта.

---
+++ tech_lab1_task3/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace tech_lab1_task3
+{
+    public class Logic
+    {
+        public static int CountLetters(string str)
+        {
+            int count = 0;
+            if (string.IsNullOrEmpty(str))
+            {
+                return -1;
+            }
+            str = str.ToLower();
+            int groupSize = 1;
+            for (int i = 1; i < str.Length; i++)
+            {
+                if (str[i] == str[i - 1]) 
+                {
+                    groupSize++;
+                }
+                else
+                {
+                    if (groupSize > 1)
+                    {
+                        count += groupSize;
+                    }
+                    groupSize = 1; 
+                }
+            }
+            if (groupSize > 1)
+            {
+                count += groupSize;
+            }
+            return count;
+        }
+    }
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку:");
+            string sentence = Console.ReadLine();
+
+            int count = Logic.CountLetters(sentence);
+
+            if (count == -1)
+            {
+                Console.WriteLine("Введена пустая строка ");
+            }
+            else
+            {
+                Console.WriteLine("Количество одинаковых соседних букв: " + count);
+            }
+        }
+    }
+}
---
+++ tech_lab1_task3Tests/LogicTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using tech_lab1_task3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tech_lab1_task3.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void StringIsNotEmptyTest()
+        {
+            string testSentence = "Hello, World!";
+
+            int result = Logic.CountLetters(testSentence);
+
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod()]
+        public void StringIsEmptyTest()
+        {
+            string testSentence = "";
+
+            int result = Logic.CountLetters(testSentence);
+
+            Assert.AreEqual(-1, result);
+        }
+    }
+}
