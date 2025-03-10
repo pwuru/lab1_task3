7733fb0911ad2439f5770acc2df29297c7a6118d
pwuru <mihail_popov2005@mail.ru>
2025-03-10 12:42:11 +0800

    Update tests

--- tech_lab1_task3Tests/LogicTests.cs
+++ tech_lab1_task3Tests/LogicTests.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using tech_lab1_task3;
 using System;
 using System.Collections.Generic;
@@ -12,23 +12,47 @@ namespace tech_lab1_task3.Tests
     public class LogicTests
     {
         [TestMethod()]
-        public void StringIsNotEmptyTest()
-        {
-            string testSentence = "Hello, World!";
-
-            int result = Logic.CountLetters(testSentence);
-
-            Assert.AreEqual(2, result);
-        }
-
-        [TestMethod()]
         public void StringIsEmptyTest()
         {
             string testSentence = "";
 
-            int result = Logic.CountLetters(testSentence);
+            var result = Logic.CountLetters(testSentence);
 
-            Assert.AreEqual(-1, result);
+            Assert.IsNull(result, "Expected result to be null for empty string input.");
+        }
+        
+        [TestMethod()]
+        public void StringHasNotIdenticalTest()
+        {
+            string testSentence = "avmo cksnz qpw";
+
+            var result = Logic.CountLetters(testSentence);
+
+            Assert.AreEqual(0, result.Count);
+        }
+
+        [TestMethod()]
+        public void StringHasNotSpacesTest()
+        {
+            string testSentence = "aaabbaa";
+
+            var result = Logic.CountLetters(testSentence);
+
+            Assert.AreEqual("Количество подряд идущих букв 'a': 3", result[0]);
+            Assert.AreEqual("Количество подряд идущих букв 'b': 2", result[1]);
+            Assert.AreEqual("Количество подряд идущих букв 'a': 2", result[2]);
+        }
+
+        [TestMethod()]
+        public void StringHasSpacesTest()
+        {
+            string testSentence = "aab baa a";
+
+            var result = Logic.CountLetters(testSentence);
+
+            Assert.AreEqual("Количество подряд идущих букв 'a': 2", result[0]);
+            Assert.AreEqual("Количество подряд идущих букв 'b': 2", result[1]);
+            Assert.AreEqual("Количество подряд идущих букв 'a': 3", result[2]);
         }
     }
 }
